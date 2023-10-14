0f5525134e76635c8bcde7876123b97910d122ec
Olexandr Savchuk <ppos19soo@gmail.com>
2023-10-14 20:30:20 +0200

    add text for check message in format for "format" method

--- bot/locales/utils.py
+++ bot/locales/utils.py
@@ -1,12 +1,14 @@
 import polib
 
 
-def get_text(msgid: str, language: str = "us") -> str:
+def get_text(msgid: str, language: str = "us", **format) -> str:
     po_file = polib.pofile(f"bot/locales/texts/{language.lower()}/messages.po")
 
     entry = po_file.find(msgid)
 
     if entry:
+        if format:
+            return entry.msgstr.format(**format)
         return entry.msgstr
 
     return msgid
