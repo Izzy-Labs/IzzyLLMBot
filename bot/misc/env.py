4749a6d856c993c9906d8eaf2af0505d26cd193b
Olexandr Savchuk <ppos19soo@gmail.com>
2023-10-14 20:26:29 +0200

    add env class for redis

--- bot/misc/env.py
+++ bot/misc/env.py
@@ -11,3 +11,11 @@ class PostgresEnv:
     USER: str = environ.get("POSTGRES_USER", "define me")
     PASSWORD: str = environ.get("POSTGRES_PASSWORD", "define me")
     DB_NAME: str = environ.get("POSTGRES_DB", "define me")
+
+
+class RedisEnv:
+    HOST: str = environ.get("REDIS_HOST", "define me")
+    PORT: int = int(environ.get("REDIS_PORT", "define me"))
+    DB: int = int(environ.get("REDIS_DB", "define me"))
+    USER: str = environ.get("REDIS_USER", "define me")
+    PASSWORD: str | None = environ.get("REDIS_PASSWORD", None)
