21e1075148d294897d345c7a7b1ffa6fb5fe981b
Olexandr Savchuk <ppos19soo@gmail.com>
2023-10-15 13:55:15 +0200

    update middleware

--- telegram/bot/misc/pg_middleware.py
+++ telegram/bot/misc/pg_middleware.py
@@ -25,6 +25,7 @@ class DatabaseMiddleware(BaseMiddleware):
     async def on_pre_process_message(self, message: types.Message, data: dict) -> None:
         """
         This method is called before processing the message.
+        :param data:
         :param message:
         :return:
         """
@@ -34,10 +35,31 @@ class DatabaseMiddleware(BaseMiddleware):
     async def on_post_process_message(self, message: types.Message, result, data: dict) -> None:
         """
         This method is called after processing the message.
+        :param data:
+        :param result:
         :param message:
         :return:
         """
-
         conn = message.pg_conn
-        if conn:
-            self.conn_pool.putconn(conn)
+        self.conn_pool.putconn(conn)
+
+    async def on_pre_process_callback_query(self, callback_query: types.CallbackQuery, data: dict) -> None:
+        """
+        This method is called before processing the callback query.
+        :param data:
+        :param callback_query:
+        :return:
+        """
+        conn = self.conn_pool.getconn()
+        callback_query.pg_conn = conn
+
+    async def on_post_process_callback_query(self, callback_query: types.CallbackQuery, result, data: dict) -> None:
+        """
+        This method is called after processing the callback query.
+        :param data:
+        :param result:
+        :param callback_query:
+        :return:
+        """
+        conn = callback_query.pg_conn
+        self.conn_pool.putconn(conn)
--- telegram/bot/misc/redis_middleware.py
+++ telegram/bot/misc/redis_middleware.py
@@ -5,12 +5,29 @@ from bot.misc.env import RedisEnv
 
 
 class RedisMiddleware(BaseMiddleware):
-
+    """
+    Middleware for adding redis connection to message
+    """
     def __init__(self):
         super().__init__()
         redis_url = f"redis://{RedisEnv.USER}:{RedisEnv.PASSWORD}@{RedisEnv.HOST}:{RedisEnv.PORT}"
         self.pool = ConnectionPool.from_url(redis_url, max_connections=10)
         self.redis = Redis(connection_pool=self.pool)
 
-    async def on_pre_process_message(self, message, data: dict):
+    async def on_pre_process_message(self, message, data: dict) -> None:
+        """
+        Add redis connection to message
+        :param message:
+        :param data:
+        :return:
+        """
         message.redis = self.redis
+
+    async def on_pre_process_callback_query(self, callback_query, data: dict) -> None:
+        """
+        Add redis connection to callback_query
+        :param callback_query:
+        :param data:
+        :return:
+        """
+        callback_query.redis = self.redis
